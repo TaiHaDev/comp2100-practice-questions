a1725891697ac1b961041d0d1ad4ea37bb46c43f
Llew Reilly <llew.reilly@anu.edu.au>
2023-03-24 15:26:13 +1100

    added s2 2022 midsem

---
+++ midsemester-practice-questions/Q10/VendingMachine.java
@@ -0,0 +1,23 @@
+
+public class VendingMachine {
+
+	private State state = new Idle(this);
+
+	public State getState() {
+
+		// START YOUR CODE
+
+
+
+		// END YOUR CODE
+	}
+
+	public void setState(State state) {
+		
+		// START YOUR CODE
+		
+
+		
+		// END YOUR CODE
+	}
+}
